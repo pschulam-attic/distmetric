d07b05ca0ee116e5fb2cc47f810e4fd96885ff6a
Peter Schulam <pschulam@gmail.com>
2013-09-30 15:51:30 -0400

    Clean up nadaraya watson estimation code.

--- src/nadarayawatson.r
+++ src/nadarayawatson.r
@@ -1,47 +1,44 @@
-smoothing_matrix <- function(D, h) {
-  L <- exp(- D / h)
-  L <- t(apply(L, 1, function(r) r / sum(r)))
-}
-
-smoothing_loocv <- function(D, Y, h) {
-  L <- smoothing_matrix(D, h)
+dist2 <- function(X, method, A) {
   
-  diff <- Y - (L %*% Y)
-  denom <- 1 - diag(L)
-  
-  mean((diff / denom)^2)
-}
-
-mahal_dist <- function(X, A) {
-  n <- nrow(X)
-  D <- matrix(0, nrow=n, ncol=n)
-
-  for (i in 1:n) {
-    mdist <- mahalanobis(X[-i, ], center=as.numeric(X[i, ]), cov=A, inverted=TRUE)
-    D[i, -i] <- mdist
+  mdist <- function(i) {
+    d <- rep(0, nrow(X))
+    d[-i] <- mahalanobis(X[-i, ], center=as.numeric(X[i, ]), cov=A, inverted=TRUE)
+    sqrt(d)
   }
-
-  sqrt(D)
-}
-
-nadarayawatson <- function(X, Y, disttype="euclidean", A) {
-  distopts <- c("euclidean")
-  mahal <- c("mahalanobis")
-
-  if (disttype %in% distopts) {
-    D <- as.matrix(dist(X, method=disttype))
-  } else if (disttype %in% mahal) {
-    D <- mahal_dist(X, A)
+  
+  if (method == "mahalanobis") {
+    t(sapply(1:nrow(X), mdist))
   } else {
-    stop(sprintf("Invalid distance option: %s", disttype))
+    dist(X, method=method)
+  }
+}
+
+nadarayawatson <- function(X, Y, D) {
+
+  smoother <- function(h) {
+    L <- exp(-D / h)
+    t(apply(L, 1, function(r) r / sum(r)))
+  }
+  
+  loocv <- function(h) {
+    L <- smoother(h)
+    mean( (Y - (L %*% Y))^2 / (1 - diag(L))^2 )
   }
 
-  h <- c(0.01, 0.1, 1:100)
-  loocvs <- sapply(h, function(h) smoothing_loocv(D, Y, h))
-  
-  L <- smoothing_matrix(D, h[order(loocvs)][1])
+  hs <- 1:100
+  loo_mse <- sapply(hs, loocv)
+  h <- hs[order(loo_mse)][1]
 
-  structure(list(X=X, Y=Y, h=h, loocvs=loocvs, L=L), class="nadarayawatson")
+  L <- smoother(h)
+
+  structure(list(
+      X = X,
+      Y = Y,
+      D = D,
+      h = h,
+      L = L,
+      loocvmse = loo_mse[order(loo_mse)][1]
+      ), class = "nadarayawatson")
 }
 
 predict.nadarayawatson <- function(nwfit) {
@@ -54,4 +51,27 @@ loocv_mse <- function(nw) {
   lcv <- nw$loocvs
   lcv <- lcv[!is.nan(lcv)]
   min(lcv, na.rm=TRUE)
+}
+
+stderr <- function(nwfit, nboot=500) {
+  n <- length(nwfit$Y)
+  means <- rep(0, n)
+  stderrs <- rep(0, n)
+
+  for (i in seq_len(n)) {
+    bootfn <- function() {
+      idx <- sample(n-1, n-1, replace=TRUE)
+      d <- nwfit$D[i, -i][idx]
+      w <- d / sum(d)
+      y <- nwfit$Y[-i][idx]
+      
+      sum(w * y)
+    }
+
+    b <- replicate(nboot, bootfn())
+    means[i] <- mean(b)
+    stderrs[i] <- sd(b)
+  }
+
+  list(means=means, stderrs=stderrs)
 }
